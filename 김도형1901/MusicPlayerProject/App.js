d7992f44efab1558fd856ddcc58045b738bce586
ehgud0670 <kimdo2297@gmail.com>
2019-09-20 22:30:58 +0900

    React-native 'MusicPlayerProject 생성'

---
+++ 김도형1901/MusicPlayerProject/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World! React-Native로 크로스 플랫폼 뮤직 플레이어 앱 만들기</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
